771ec4588df9e48ef953aee835eff45a155ea116
Paolo Cuffiani <paolo.cuffiani@gmail.com>
2022-10-13 09:47:40 +0200

    [minor] style: add missing trailing comma

--- src/Model/Behavior/ClosingDaysBehavior.php
+++ src/Model/Behavior/ClosingDaysBehavior.php
@@ -65,7 +65,7 @@ class ClosingDaysBehavior extends Behavior
                     : $object
             )
             : $results,
-            Query::PREPEND
+            Query::PREPEND,
         );
     }
 
